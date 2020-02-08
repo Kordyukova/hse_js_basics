4b9dcd24fb1fba921efb9606da1f7703693992c2
Marianna <marianna-vertinskaya@mail.ru>
2020-02-08 13:13:21 +0300

    .

--- 04/animation.js
+++ 04/animation.js
@@ -7,9 +7,27 @@ let box = document.querySelector(".box")
 function paintItBlack() {
   box.style.backgroundColor = "black"
 }
+
 function makeCircle() {
   box.style.borderRadius = "200px"
   if (box.style.borderRadius === "200px") {
     box.style.borderRadius = 0
   }
+}
+
+function move() {
+  box.style.transform = "translate(100px, 100px)"
+}
+
+function makeBlinking() {
+  box.style.animation = "blink 0.2s linear infinite"
+}
+
+function addText() {
+
+}
+
+function changeColor() {
+  color = getElementById('colorInput').value;
+  box.style.backgroundColor = color;
 }
